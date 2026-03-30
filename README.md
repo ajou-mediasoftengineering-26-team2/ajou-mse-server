d97e318c6bbf97c196223d037628be2bfab5686c
TandyRum1024 <yubin2892@gmail.com>
2026-03-30 20:40:02 +0900

    [🎇] Spring boot setup

---
+++ README.md
@@ -0,0 +1,9 @@
+# MSE 26-1 - Backend server
+---
+
+# Stack
+
+- Spring boot
+- Java
+  - Java (JDK) 25
+- Groovy
---
+++ src/main/java/team2/mse/ajou/server/AjouMseServerApplication.java
@@ -0,0 +1,13 @@
+package team2.mse.ajou.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AjouMseServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AjouMseServerApplication.class, args);
+    }
+
+}
