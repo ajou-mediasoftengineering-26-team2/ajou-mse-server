75645ff4490181828c2ca662b7031bbd70f5bcb6
TandyRum1024 <yubin2892@gmail.com>
2026-04-15 11:32:37 +0900

    [🎨] 코멘트 추가

--- src/main/java/team2/mse/ajou/server/domain/auth/controller/AuthController.java
+++ src/main/java/team2/mse/ajou/server/domain/auth/controller/AuthController.java
@@ -22,6 +22,13 @@ public class AuthController {
     @Autowired
     private MatchmakingRepository matchmakingRepository;
 
+    /**
+     * 주어진 닉네임으로 로그인하고, 로비에 입장하거나 새로운 로비를 생성합니다.
+     * 플레이어 고유식별자인 플레이어 토큰과, 로비 고유식별자인 로비 ID, 그리고 로비가 새로 생성되어 다른 플레이어 대기가 필요한지를 응답으로 내려줍니다.
+     *
+     * @param req Request body
+     * @return Response body
+     */
     @PostMapping("/player")
     public PostPlayerResponse postPlayer(
             @RequestBody PostPlayerRequest req
@@ -52,13 +59,18 @@ public class AuthController {
         }
 
         try {
+            // createLobby() 도 실패하면 무슨 일이 생겨서 로비를 참가할수도 새로 생성할수도 없는 상황인 것... 이거는 버그일 가능성이 커요
             if (lobbyId == null) {
                 throw new ApiError(5001, "로비 검색에 실패했습니다.");
             }
-            if (!matchmakingRepository.joinLobby(playerToken, lobbyId)) {
+
+            // joinLobby()가 실패하는 것도 동일한 이치
+            boolean result = matchmakingRepository.joinLobby(playerToken, lobbyId);
+            if (!result) {
                 throw new ApiError(5002, "로비 참가에 실패했습니다.");
             }
         } catch (ApiError _) {
+            // 뭐가되었든 로비 참가에 실패하면 자동으로 로그아웃 시켜줍시다
             authRepository.logout(playerToken);
         }
 
@@ -70,6 +82,11 @@ public class AuthController {
         return res;
     }
 
+    /**
+     * 로그인 된 플레이어 토큰으로 로그아웃 합니다. 참가중인 로비가 있으면 나갑니다.
+     *
+     * @param req Request body
+     */
     @DeleteMapping("/player")
     public void deletePlayer(
             @RequestBody DeletePlayerRequest req
@@ -89,6 +106,13 @@ public class AuthController {
         System.out.printf("Player `%s` left the game!\n", username);
     }
 
+    /**
+     * 주어진 닉네임이 이미 사용 중인지 확인합니다.
+     * 사용 가능한지 여부를 응답으로 내려줍니다.
+     *
+     * @param req Request body
+     * @return Response body
+     */
     @GetMapping("/player")
     public GetPlayerResponse getPlayer(
             @RequestBody GetPlayerRequest req
