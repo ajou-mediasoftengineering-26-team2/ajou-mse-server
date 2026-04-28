c563009e7e37823830144477f2e867763f062175
TandyRum1024 <yubin2892@gmail.com>
2026-04-28 21:48:33 +0900

    [🐝] HOTFIX: FirebaseConfig 경로 수정

--- src/main/java/team2/mse/ajou/server/FirebaseConfig.java
+++ src/main/java/team2/mse/ajou/server/FirebaseConfig.java
@@ -8,6 +8,7 @@ import com.google.firebase.database.*;
 import jakarta.annotation.PostConstruct;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 
 import java.io.FileInputStream;
 
@@ -17,11 +18,11 @@ public class FirebaseConfig {
     @PostConstruct
     public void init() throws Exception {
         if (FirebaseApp.getApps().isEmpty()) {
-            FileInputStream serviceAccount =
-                    new FileInputStream("src/main/resources/ajou-mse-firebase-adminsdk-fbsvc-1dee6ea80f.json");
+            // Classpath 에서 파일 불러오기 (-> resources 폴더 안에서 파일을 알아서 잘 찾게 만듭니다.)
+            ClassPathResource serviceAccountJson = new ClassPathResource("ajou-mse-firebase-adminsdk-fbsvc-1dee6ea80f.json");
 
             FirebaseOptions options = FirebaseOptions.builder()
-                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                    .setCredentials(GoogleCredentials.fromStream(serviceAccountJson.getInputStream()))
                     .setDatabaseUrl("https://ajou-mse-default-rtdb.asia-southeast1.firebasedatabase.app/")
                     .build();
 
