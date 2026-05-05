778b0ca263c9bea464c1b8c93bf4403c1a737df0
TandyRum1024 <yubin2892@gmail.com>
2026-05-05 09:33:41 +0900

    [➕] 테스트 페이지용 CORS 추가

---
+++ src/main/java/team2/mse/ajou/server/WebCorsConfig.java
@@ -0,0 +1,17 @@
+package team2.mse.ajou.server;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebCorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:6767", "https://ajou-mediasoftengineering-26-team2.github.io/ajou-mse-backend-page") // 테스트페이지
+                .allowedMethods("GET", "POST", "DELETE", "PUT", "PATCH", "DELETE")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
