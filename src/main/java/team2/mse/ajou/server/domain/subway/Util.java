963a008e535d88c1e00c2c25e7d15a21b4176137
이준상 <66434787+samgashyeong@users.noreply.github.com>
2026-05-10 16:45:00 +0900

    fix : 지하철 역이 Unknown으로 뜨는 버그 수정

---
+++ src/main/java/team2/mse/ajou/server/domain/subway/Util.java
@@ -0,0 +1,7 @@
+package team2.mse.ajou.server.domain.subway;
+//202322158 이준상
+
+//Set Unknown as Gangnam Station when spring can't find the subway
+public class Util {
+    public static final String UNKNOWN = "GANGNAM";
+}
