6139edaca7e6be352ad475f0483dbe268f332253
sepilboard <sepilboard@gmail.com>
2026-05-10 16:45:33 +0900

    커밋테스트

--- src/main/java/team2/mse/ajou/server/TestController.java
+++ src/main/java/team2/mse/ajou/server/TestController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
+// Test
 @RestController
 public class TestController {
     @GetMapping("/test")
