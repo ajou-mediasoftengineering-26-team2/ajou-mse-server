8f0371b4e3ad3eb1c28fce26541b6f57fae7d255
sepilboard <sepilboard@gmail.com>
2026-05-19 19:25:58 +0900

    Added: item, perk, elemental enum class

---
+++ src/main/java/team2/mse/ajou/server/domain/shared/match/HAND_ELEMENTAL.java
@@ -0,0 +1,15 @@
+package team2.mse.ajou.server.domain.shared.match;
+
+/**
+ * Hand Elemental.
+ *
+ * @author Junseo Hwang 202322128
+ */
+public enum HAND_ELEMENTAL {
+    FIRE,       // 불
+    LIGHTNING,  // 번개
+    WATER,      // 물
+    PLANT,      // 풀(식물)
+    WIND,       // 바람
+    POISON,     // 독
+}
---
+++ src/main/java/team2/mse/ajou/server/domain/shared/match/PERK.java
@@ -0,0 +1,22 @@
+package team2.mse.ajou.server.domain.shared.match;
+
+/**
+ * Perk.
+ *
+ * @author Junseo Hwang 202322128
+ */
+public enum PERK {
+    IRON_FIST,          // 강철 주먹
+    VAMPIRISM,          // 흡혈귀
+    THRIFTY,            // 절약가
+    GRIT,               // 근성
+    TAUNT,              // 도발
+    FOCUS,              // 집중
+    LUCK,               // 행운
+    UNYIELDING,         // 불굴
+    INSERT_MASTER,      // 찌르기 달인
+    FLEXIBLE_HANDS,     // 유연한 손
+    FLEXIBLE_MIND,      // 유연한 사고
+    RICH,               // 부자
+    WISE_INVESTOR       // 현명한 투자자
+}
