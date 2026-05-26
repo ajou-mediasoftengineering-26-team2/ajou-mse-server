9a62b1745ae43026afd37297d75f6d06b1469d6a
sepilboard <sepilboard@gmail.com>
2026-05-26 13:33:34 +0900

    Added: Round Controller (라운드 시작 끝 애니메이션 ack)

---
+++ src/main/java/team2/mse/ajou/server/domain/round/service/RoundService.java
@@ -0,0 +1,15 @@
+package team2.mse.ajou.server.domain.round.service;
+
+import java.util.UUID;
+
+public class RoundService implements IRoundService {
+    @Override
+    public void receiveRoundStart(UUID playerId) {
+
+    }
+
+    @Override
+    public void receiveRoundEnd(UUID playerId) {
+        
+    }
+}
