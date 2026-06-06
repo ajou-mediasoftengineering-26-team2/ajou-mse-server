9459ef83284bedcc6a0ab9a474e407cd57ecd864
sepilboard <sepilboard@gmail.com>
2026-06-06 23:05:06 +0900

    Fixed: import 문제로 인한 빌드 오류 해결

--- src/main/java/team2/mse/ajou/server/domain/shared/match/model/DefendData.java
+++ src/main/java/team2/mse/ajou/server/domain/shared/match/model/DefendData.java
@@ -5,7 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.springframework.data.annotation.Id;
+import jakarta.persistence.Id;
 import team2.mse.ajou.server.domain.shared.match.HAND_ELEMENTAL;
 import team2.mse.ajou.server.domain.shared.match.ITEM_CODE;
 import team2.mse.ajou.server.domain.shared.match.PERK;
